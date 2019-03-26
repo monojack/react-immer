a3580dddb8988877fab109814580d41476b8e8dd
Ionut Achim <achimvionut@gmail.com>
2019-03-26 22:39:46 +0200

    Improve effect hook

--- src/useImmerHook.js
+++ src/useImmerHook.js
@@ -8,13 +8,10 @@ export default function useImmerHook (spec) {
   const [ state, setState, ] = useState(applySpec(spec)(state$.value))
 
   useEffect(() => {
-    const update = applySpec(spec)(state$.value)
-    if (!shallowEqual(state, update)) setState(update)
-
     const sub = state$.subscribe({
-      next: v => {
-        const prevState = applySpec(spec)(state$.value)
-        const nextState = applySpec(spec)(v)
+      next: (next, prev) => {
+        const prevState = applySpec(spec)(prev)
+        const nextState = applySpec(spec)(next)
 
         if (!shallowEqual(prevState, nextState)) {
           setState(nextState)
